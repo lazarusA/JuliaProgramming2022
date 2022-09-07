9398265f7290b169a154fbd9eaa357e225f8f9c9
Felix Cremer <fcremer@bgc-jena.mpg.de>
2022-09-07 10:47:57 +0200

    Add EDL Exercises without solutions

---
+++ DataHandling/datacubes_edl_excercises_wo_solutions.jl
@@ -0,0 +1,24 @@
+# 1) 
+# Calculate the temporal and spatial variance (separately) 
+# of each variable in the regional cube, using mapslices.
+
+# 2) 
+# Calculate the temporal variance for MSC and anomalies of air temperature.
+
+# 3)
+# Repeat 2, but use mapCube instead of mapslices
+
+
+# 4) 
+# Calculate a more robust correlation (see below) and 
+# compare the difference to the regular correlation we computed.
+
+# 5) 
+# Calculate an "easy" ratio of NEE and GPP  (temporal) mean(NEE)/mean(GPP), for each pixel
+
+
+# 6) 
+# Extend 5 so that you return both ratio and temporal correlation 
+# - how does outdims have to look now?
+
+
